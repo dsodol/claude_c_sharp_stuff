c8d30658ba47616738e57036d0df523deda768f1
dsodol <dsodol@yahoo.com>
2025-12-16 09:55:28 -0800

    Initial commit: Hello World

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ClaudeCSharpStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Claude!");
+        }
+    }
+}
